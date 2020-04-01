28ef56c396d85a231463974b59c5da70ea7674a7
Adrian Romero <radrian031613@gmail.com>
2020-04-01 16:56:45 -0700

    made modifications to the functions to define variables that were exported and that were called using axios.

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,6 @@
-
-
 const generateHTML = require("./generateHTML");
-// console.log(typeof generateHTML.generateHTML);
-// console.log(typeof generateHTML.colors);
+console.log(typeof generateHTML.generateHTML);
+console.log(typeof generateHTML.colors);
 const fs = require("fs"),
     convertFactory = require('electron-html-to');
 const axios = require("axios");
@@ -11,13 +9,12 @@ const inquirer = require("inquirer");
 const conversion = convertFactory({
     converterPath: convertFactory.converters.PDF
 });
-// let resumeHTML = generateHTML;
-// console.log(resumeHTML);
 
-
-let data = {};
+// let data = {};
 // console.log(data)
 
+let newHTML = generateHTML.generateHTML;
+console.log(newHTML)
 let questions = [
     {
         message: 'What is your github username?',
@@ -33,83 +30,94 @@ let questions = [
 // function generateHTML() to get called from gerenateHTML.js
 
 // function writeToFile(fileName, data) {
+    
+    // } not necessary with electron-html-to\
+// let HTML = function generateHTML(res) {
+//     return res;
+// }
 
-// } not necessary with electron-html-to
-
+let colors = generateHTML.colors;
 function init() {
-    inquirer
-    .prompt(questions)
-    .then(function ({username, color}) {
-        const queryUrl = `https://api.github.com/users/${username}`; 
-
-        axios
-            .get(queryUrl)
-            .then((res) => {    
-                // console.log(res.data)
-
+        newHTML;
+            inquirer
+            .prompt(questions)
+            .then(function ({username, color}) {
+                const queryUrl = `https://api.github.com/users/${username}`; 
                 
+                axios
+                .get(queryUrl)
+                .then((res) => {    
+                    console.log(res.data)
+                    let data = res.data;
+                    
+                    data.color = [];
+                    
+                    
+                    switch(color) {
+                        case 'green':
+                            data.color = 0;
+                            break;
+                            case 'blue':
+                                data.color = 1;
+                                break;  
+                                case 'pink':
+                                    data.color = 2;
+                                    break;
+                                    case 'red':
+                                        data.color = 3;
+                                        break;
+                                    }      
+                                    console.log(data.color)
+                                    console.log(colors);  
+                                    
+                                    let = data.username = username;
+                                    let = data.numOfRepo = res.data.public_repos;
+                                    let = data.name = res.data.name
+                                    let = data.followers = res.data.followers;
+                                    let = data.following = res.data.following;
+                                    let = data.portPic = res.data.avatar_url;
+                                    let = data.location = res.data.location;
+                                    let = data.blog = res.data.blog; 
+                                    let = data.company = res.data.company
+                                    let = data.bio = res.data.bio
+                                    console.log(data);
+                                    axios // Requires a different axios call to get stars
+                                    .get(`https://api.github.com/users/${username}/repos?per_page=100`)
+                                    .then((res) => {
+                                        // console.log(res)
+                                        data.stars = 0;
+                                        for (let i = 0; i < res.data.length; i++) { // Loop through each repository and count the number of stars
+                                            data.stars += res.data[i].stargazers_count;
+                                        }
+                                        
+                                        
+                                        // console.log(data.stars)
+                                        // let resumeHTML = JSON.stringify(generateHTML.generateHTML());
 
-                switch(color) {
-                    case 'green':
-                        data.color = 0;
-                        break;
-                    case 'blue':
-                        data.color = 1;
-                        break;  
-                    case 'pink':
-                        data.color = 2;
-                        break;
-                    case 'red':
-                        data.color = 3;
-                        break;
-                }      
-                // console.log(data.color)  
 
-                data.username = username;
-                data.numOfRepo = res.data.public_repos;
-                data.name = res.data.name
-                data.followers = res.data.followers;
-                data.following = res.data.following;
-                data.portPic = res.data.avatar_url;
-                data.location = res.data.location;
-                data.blog = res.data.blog; 
-                data.company = res.data.company
-                data.bio = res.data.bio
 
-                axios // Requires a different axios call to get stars
-                    .get(`https://api.github.com/users/${username}/repos?per_page=100`)
-                    .then((res) => {
-                        // console.log(res)
-                        data.stars = 0;
-                        for (let i = 0; i < res.data.length; i++) { // Loop through each repository and count the number of stars
-                            data.stars += res.data[i].stargazers_count;
-                        }
-                        
 
-                        // console.log(data.stars)
 
-                        // let resumeHTML = generateHTML(data);
-                        // // console.log(resumeHTML)
 
-                        conversion({ html: generateHTML }, function(err, result) {
-                            if (err) {
-                              return console.error(err);
-                            }
-                           
-                            console.log(result.numberOfPages);
-                            console.log(result.logs);
-                            result.stream.pipe(fs.createWriteStream('./resume.pdf'));
-                            conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+
+
+                                        /*this is returning resumeHTML as colors but redefined to case switch choice*/
+                                        
+                                        console.log(newHTML(data));
+                                        conversion({ html: newHTML }, function(err, result) {
+                                            if (err) {
+                                  return console.error(err);
+                                }
+                            //    console.log(result);
+                            //     console.log(result.numberOfPages);
+                            //     console.log(result.logs);
+                                result.stream.pipe(fs.createWriteStream('./resume.pdf'));
+                                conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+                                    })
+                                    
                           });
-                    })
-
-                
-
-
-               
-
-            })
-    })
-}
+                })
+        })
+    }
 
 init();

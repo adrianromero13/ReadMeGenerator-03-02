64101eeb322e6bb798bb7436c0cf9f3ea932fde8
Adrian Romero <radrian031613@gmail.com>
2020-04-04 15:36:03 -0700

    changed the export method on generateHTML.js file. Changed electron dependecy to an older version. Changed syntactical errors on the index.js file. Generated a pdf that was saved into the root folder.

--- Develop/generateHTML.js
+++ Develop/generateHTML.js
@@ -1,37 +1,35 @@
 
-module.exports = {
-
-
-  colors: {
-    green: {
+  const colors =  [
+     {
       wrapperBackground: "#E6E1C3",
       headerBackground: "#C1C72C",
       headerColor: "black",
       photoBorderColor: "#black"
     },
-    blue: {
+     {
       wrapperBackground: "#5F64D3",
       headerBackground: "#26175A",
       headerColor: "white",
       photoBorderColor: "#73448C"
     },
-    pink: {
+     {
       wrapperBackground: "#879CDF",
       headerBackground: "#FF8374",
       headerColor: "white",
       photoBorderColor: "#FEE24C"
     },
-    red: {
+     {
       wrapperBackground: "#DE9967",
       headerBackground: "#870603",
       headerColor: "white",
       photoBorderColor: "white"
     }
-  },
+
+  ];
 
 
-  generateHTML: function (data) {
- // Generates HTML based on data given to create a PDF resume
+  // Generates HTML based on data given to create a PDF resume
+  function generateHTML(data) {
       return `<!DOCTYPE html>
     <html lang="en">
        <head>
@@ -228,9 +226,11 @@ module.exports = {
     
             </div>
     </body>`
-  },
-};
-
+  }
       //should export to wherever file this funciton gets called in that requires this file
     //  exports.colors = colors;
     //  exports.generateHTML = generateHTML;
+module.exports = {
+  colors,
+  generateHTML
+};
--- Develop/index.js
+++ Develop/index.js
@@ -1,20 +1,23 @@
-const generateHTML = require("./generateHTML");
-console.log(typeof generateHTML.generateHTML);
-console.log(typeof generateHTML.colors);
+const doc = require("./generateHTML");
+// console.log(typeof generateHTML.generateHTML);
+// console.log(typeof generateHTML.colors);
 const fs = require("fs"),
     convertFactory = require('electron-html-to');
 const axios = require("axios");
 const inquirer = require("inquirer");
+const electron = require("electron");
+// html
 
 const conversion = convertFactory({
-    converterPath: convertFactory.converters.PDF
+    converterPath: convertFactory.converters.PDF,
+    allowLocalFilesAccess: true
 });
 
 // let data = {};
 // console.log(data)
 
-let newHTML = generateHTML.generateHTML;
-console.log(newHTML)
+// let newHTML = generateHTML;
+// console.log(newHTML)
 let questions = [
     {
         message: 'What is your github username?',
@@ -30,94 +33,90 @@ let questions = [
 // function generateHTML() to get called from gerenateHTML.js
 
 // function writeToFile(fileName, data) {
-    
-    // } not necessary with electron-html-to\
+
+// } not necessary with electron-html-to\
 // let HTML = function generateHTML(res) {
 //     return res;
 // }
 
-let colors = generateHTML.colors;
+// let colors = generateHTML.colors;
 function init() {
-        newHTML;
-            inquirer
-            .prompt(questions)
-            .then(function ({username, color}) {
-                const queryUrl = `https://api.github.com/users/${username}`; 
-                
-                axios
+    // newHTML;
+    inquirer
+        .prompt(questions)
+        .then(function ({ username, color }) {
+            const queryUrl = `https://api.github.com/users/${username}`;
+
+            axios
                 .get(queryUrl)
-                .then((res) => {    
+                .then((res) => {
                     console.log(res.data)
                     let data = res.data;
-                    
-                    data.color = [];
-                    
-                    
-                    switch(color) {
+                    // console.log(res.data);
+
+                    // data.color = [];
+
+
+                    switch (color) {
                         case 'green':
                             data.color = 0;
                             break;
-                            case 'blue':
-                                data.color = 1;
-                                break;  
-                                case 'pink':
-                                    data.color = 2;
-                                    break;
-                                    case 'red':
-                                        data.color = 3;
-                                        break;
-                                    }      
-                                    console.log(data.color)
-                                    console.log(colors);  
-                                    
-                                    let = data.username = username;
-                                    let = data.numOfRepo = res.data.public_repos;
-                                    let = data.name = res.data.name
-                                    let = data.followers = res.data.followers;
-                                    let = data.following = res.data.following;
-                                    let = data.portPic = res.data.avatar_url;
-                                    let = data.location = res.data.location;
-                                    let = data.blog = res.data.blog; 
-                                    let = data.company = res.data.company
-                                    let = data.bio = res.data.bio
-                                    console.log(data);
-                                    axios // Requires a different axios call to get stars
-                                    .get(`https://api.github.com/users/${username}/repos?per_page=100`)
-                                    .then((res) => {
-                                        // console.log(res)
-                                        data.stars = 0;
-                                        for (let i = 0; i < res.data.length; i++) { // Loop through each repository and count the number of stars
-                                            data.stars += res.data[i].stargazers_count;
-                                        }
-                                        
-                                        
-                                        // console.log(data.stars)
-                                        // let resumeHTML = JSON.stringify(generateHTML.generateHTML());
+                        case 'blue':
+                            data.color = 1;
+                            break;
+                        case 'pink':
+                            data.color = 2;
+                            break;
+                        case 'red':
+                            data.color = 3;
+                            break;
+                        default:
+                            console.log('');
+                    }
+                    // console.log(data.color)
+                    // console.log(colors);  
 
+                    data.username = username;
+                    data.numOfRepo = res.data.public_repos;
+                    data.name = res.data.name
+                    data.followers = res.data.followers;
+                    data.following = res.data.following;
+                    data.portPic = res.data.avatar_url;
+                    data.location = res.data.location;
+                    data.blog = res.data.blog;
+                    data.company = res.data.company
+                    data.bio = res.data.bio
+                    // console.log(data);
+                    axios // Requires a different axios call to get stars
+                        .get(`https://api.github.com/users/${username}/repos?per_page=100`)
+                        .then((res) => {
+                            // console.log(res)
+                            data.stars = 0;
+                            for (let i = 0; i < res.data.length; i++) { // Loop through each repository and count the number of stars
+                                data.stars += res.data[i].stargazers_count;
+                            }
+                            let newHTML = doc.generateHTML(data);
+                            // create html file
 
+                            // console.log(data.stars)
+                            // window.ELECTRON_HTML_TO_READY = true;
+                            console.log(newHTML);
+                            conversion({ html: newHTML }, function (err, result) {
 
+                                if (err) {
+                                    return console.error(err);
 
-
-
-
-
-                                        /*this is returning resumeHTML as colors but redefined to case switch choice*/
-                                        
-                                        console.log(newHTML(data));
-                                        conversion({ html: newHTML }, function(err, result) {
-                                            if (err) {
-                                  return console.error(err);
                                 }
-                            //    console.log(result);
-                            //     console.log(result.numberOfPages);
-                            //     console.log(result.logs);
+                                //    console.log(result);
+                                //     console.log(result.numberOfPages);
+                                //     console.log(result.logs);
                                 result.stream.pipe(fs.createWriteStream('./resume.pdf'));
                                 conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
-                                    })
-                                    
-                          });
+                            })
+
+                        });
                 })
         })
-    }
+}
 
 init();
